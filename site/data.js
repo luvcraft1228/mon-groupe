bee5dad6919a70122fafd49d40ef396f59783459
David Brais <2095059@cshawi.ca>
2021-06-08 13:08:53 -0400

    travail sur delete confirm

---
+++ site/data.js
@@ -0,0 +1,65 @@
+let characterStorage = window.localStorage;
+
+let data = {
+  nextId: 5,
+  characters: [
+    {
+      id: 0,
+      avatarUrl:
+        "https://i.pinimg.com/236x/2a/b2/bf/2ab2bf57c9326c85f1805f08bb21e3e1.jpg",
+      name: "Billy",
+      xp: 10600,
+      classes: ["voleur"],
+    },
+    {
+      id: 1,
+      avatarUrl:
+        "https://i.pinimg.com/originals/cf/61/d5/cf61d563013cf829deb56e10b32e681b.jpg",
+      name: "Jess",
+      xp: 1500,
+      classes: ["bard", "sorcerer"],
+    },
+    {
+      id: 2,
+      avatarUrl:
+        "https://www.hireanillustrator.com/i/images/2018/07/Melanie_gnomeportrait_finalsm-600x750.jpg",
+      name: "Sammy the wise",
+      xp: 6040,
+      classes: ["wizard", "arcanist", "librarian"],
+    },
+    {
+      id: 3,
+      avatarUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy5IBwH6CkVL-aNYpAXAkNvE2RsKDVmCTx0NSKzo-uEidh1wFwlLRdfdDgEgcxIIHw1_8&usqp=CAU",
+      name: "Sir Killalot",
+      xp: 10560,
+      classes: ["figther", "dragon priest"],
+    },
+    {
+      id: 4,
+      avatarUrl:
+        "https://i.pinimg.com/originals/aa/a1/f4/aaa1f44fe31bd58585a124970bc0c93b.jpg",
+      name: "Grum",
+      xp: 22000,
+      classes: ["barbarian", "war chief"],
+    },
+  ],
+};
+console.log(characterStorage);
+//writeToLocalStorage();
+/*---- verification de premiere utilisation ----*/
+if (characterStorage.getItem("data") == null) {
+  writeToLocalStorage();
+} else {
+  data = JSON.parse(characterStorage.getItem("data"));
+}
+
+function writeToLocalStorage() {
+  let dataSerialized = JSON.stringify(data);
+  characterStorage.setItem("data", dataSerialized);
+}
+
+function resetDb(){
+  characterStorage.removeItem("data");
+  location.reload();
+}
--- site/script.js
+++ site/script.js
@@ -1,97 +1,44 @@
 const inputs = document.querySelectorAll("#inputs div>*");
 const nameInput = inputs[0];
 const xpInput = inputs[1];
-const avatarUrlInput = inputs[2]; 
+const avatarUrlInput = inputs[2];
 const classesInput = inputs[3];
-const submit = inputs[4];
-const modif = inputs[5];
+const buttons = document.querySelectorAll("#inputs button");
+const submit = buttons[0];
+const modif = buttons[1];
+const cancel = buttons[2];
 
-const addNode = document.querySelector("thead");
+const addNode = document.querySelector("tbody");
 
 submit.addEventListener("click", readInputs);
 modif.addEventListener("click", commitChanges);
-/* ---- Local Storage ---- */
+cancel.addEventListener("click", cancelChanges);
 
-let characterStorage = window.localStorage;
-let characters = [
-  {
-    id: 0,
-    avatarUrl:
-      "https://i.pinimg.com/236x/2a/b2/bf/2ab2bf57c9326c85f1805f08bb21e3e1.jpg",
-    name: "Billy",
-    xp: 10600,
-    classes: ["voleur"],
-  },
-  {
-    id: 1,
-    avatarUrl:
-      "https://i.pinimg.com/originals/cf/61/d5/cf61d563013cf829deb56e10b32e681b.jpg",
-    name: "Jess",
-    xp: 1500,
-    classes: ["bard", "sorcerer"],
-  },
-  {
-    id: 2,
-    avatarUrl:
-      "https://www.hireanillustrator.com/i/images/2018/07/Melanie_gnomeportrait_finalsm-600x750.jpg",
-    name: "Sammy the wise",
-    xp: 6040,
-    classes: ["wizard", "arcanist", "librarian"],
-  },
-  {
-    id: 3,
-    avatarUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy5IBwH6CkVL-aNYpAXAkNvE2RsKDVmCTx0NSKzo-uEidh1wFwlLRdfdDgEgcxIIHw1_8&usqp=CAU",
-    name: "Sir Killalot",
-    xp: 10560,
-    classes: ["figther", "dragon priest"],
-  },
-  {
-    id: 4,
-    avatarUrl:
-      "https://i.pinimg.com/originals/aa/a1/f4/aaa1f44fe31bd58585a124970bc0c93b.jpg",
-    name: "Grum",
-    xp: 22000,
-    classes: ["barbarian", "war chief"],
-  },
-];
-//writeToLocalStorage();
-/*---- verification de premiere utilisation ----*/
-if (characterStorage.getItem("characters") == null) {
-    alert("null");
-  writeToLocalStorage();
-}else{
-  characters = JSON.parse(characterStorage.getItem("characters"))
-}
+/*--- initialisation du tableau avec les donnee stockees ---*/
 
-
-function writeToLocalStorage(){
-    let charactersSerialized = JSON.stringify(characters);
-    characterStorage.setItem("characters", charactersSerialized);
-    
-}
-
-
-/*--- initialisation du tableau avec les donnee stocker ---*/
-characters.forEach(element => print(element))
+data.characters.forEach((element) => print(element));
 
 /*---- ajout d'un character a la liste ---- */
 
-function add(character){
-  characters.push(character);
-  writeToLocalStorage(); 
+function add(character) {
+  data.characters.push(character);
+
+  writeToLocalStorage();
 }
 
 /* ---- Creation d'une ligne ---- */
-function print(character){
-   
-  
-   let newTR = document.createElement("tr");
-   let lvlCount = levelCount(character.xp);
-   let lvl = lvlCount.lvl;
-   let toNextLvl = (character.xp/lvlCount.xpAcc*100); 
-   newTR.id = character.id;
-    newTR.innerHTML = `
+function print(character) {
+  let newTR = document.createElement("tr");
+  newTR.id = character.id;
+  newTR.innerHTML = writeInnerHTML(character);
+  addNode.appendChild(newTR);
+}
+
+function writeInnerHTML(character) {
+  let lvlCount = levelCount(character.xp);
+  let lvl = lvlCount.lvl;
+  let toNextLvl = (character.xp / lvlCount.xpAcc) * 100;
+  return `
     <td><img src="${character.avatarUrl}" alt=""></td>
     <td>${character.name}</td>
     <td>${character.classes}</td>
@@ -99,126 +46,167 @@ function print(character){
     <td>${character.xp}</td>
     <td><div class="progress-bar" ><div class="progress" style="width:${toNextLvl}%"></div></div></td>
     <td><i class="fas fa-cog" onclick="modify(this)"></i></td>
-    <td><i class="fas fa-times"></i></td>
+    <td><i class="fas fa-times"onclick="deleteRow(this)"></i></td>
     `;
-
-    addNode.appendChild(newTR);
-};
+}
 
 /* ---- Lecture des entrees & appel fonction affichage ----*/
-function readInputs(){
-  if(validateInputs()){
-
+function readInputs() {
+  if (validateInputs()) {
     let newCharacter = {
-      id : characters.length-1,
-        name : nameInput.value,
-        xp : xpInput.value,
-        avatarUrl : avatarUrlInput.value,
-        classes : classesReader()
+      id: data.nextId++,
+      name: nameInput.value,
+      xp: xpInput.value,
+      avatarUrl: avatarUrlInput.value,
+      classes: classesParser(),
     };
-    
+
     add(newCharacter);
     print(newCharacter);
     clear();
   }
-    
 }
 
-function classesReader(){
-    let classString = classesInput.value;
-    let classes = classString.split("\n");
-   
-   return classes;
+function classesParser() {
+  //classes parser
+  let classString = classesInput.value;
+  let classes = classString.split("\n");
+
+  return classes;
 }
 
-function clear(){
-  inputs.forEach(input =>input.value = "");
+function clear() {
+  inputs.forEach((input) => (input.value = ""));
+}
+
+function clearErrors() {
+  inputs.forEach((i) => i.classList.remove("invalid"));
 }
 
 /* ---- Calculs et validations ---*/
 const nameValidation = new RegExp(/^[A-Za-z\s]+$/);
-const urlValidation = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&=]*)/gm);
+const urlValidation = new RegExp(
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&=]*)/gm
+);
 
-function validateInputs(){
-  let valid = true;
-    if(!nameInput.value.match(nameValidation)){ //name
-      valid = false;
-      nameInput.classList.add("invalid");
-    }else nameInput.classList.remove("invalid");
-    
-    if (isNaN(xpInput.value)||xpInput.value <=0) {//xp
-      valid = false;
-      xpInput.classList.add("invalid");
-    } else xpInput.classList.remove("invalid");
-    
-    if(!avatarUrlInput.value.match(urlValidation)){ //url avatar
-      valid = false;
-      avatarUrlInput.classList.add("invalid");
-    }else avatarUrlInput.classList.remove("invalid");
-    return valid;
-    
+//this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1')
+
+nameInput.addEventListener("keydown", validateName);
+xpInput.addEventListener("input", validateXp);
+
+function validateName(event) {
+  console.log(event.key.match(nameValidation));
+  if (event.key.match(nameValidation) == null) {
+    event.preventDefault();
+  }
 }
-///Compte le niveau auquel le personnage est rendu par 
-///rapport au nombre d'xp accumule 
+function validateXp() {
+  this.value = this.value.replace(/[^0-9]/g, "").replace(/(\..*?)\..*/g, "$1");
+}
+
+function validateInputs() {
+  //let valid = true;
+  if (!nameInput.reportValidity()) {
+    nameInput.classList.add("invalid");
+    return false;
+  } else nameInput.classList.remove("invalid");
+  if (!xpInput.reportValidity()) {
+    xpInput.classList.add("invalid");
+    return false;
+  } else xpInput.classList.remove("invalid");
+
+  if (!avatarUrlInput.value.match(urlValidation)) {
+    avatarUrlInput.classList.add("invalid");
+    return false;
+  } else avatarUrlInput.classList.remove("invalid");
+  return true;
+}
+///Compte le niveau auquel le personnage est rendu par
+///rapport au nombre d'xp accumule
 ///retourne le niveau et l'experience total necessaire au prochain niveau
-function levelCount(xp){
-let count = {
-  lvl : 1,
-  xpAcc : 0
-}
- do{
-  count.xpAcc+=count.lvl*1000;
-  
-  if (count.xpAcc>xp) return count;
-  count.lvl++; 
- }while(count.xpAcc<=xp);
+function levelCount(xp) {
+  let count = {
+    lvl: 1,
+    xpAcc: 0,
+  };
+  do {
+    count.xpAcc += count.lvl * 1000;
+    if (count.xpAcc > xp) return count;
+    count.lvl++;
+  } while (count.xpAcc <= xp);
 }
 
 /*---- Modifier une ligne --- */
 
-function modify(element){
- let id = element.parentElement.parentElement.id;
- let affectedChar = characters.find(element => element.id == id)
- 
+function modify(element) {
+  let id = element.parentElement.parentElement.id;
+  let affectedChar = data.characters.find((element) => element.id == id);
   nameInput.value = affectedChar.name;
   xpInput.value = affectedChar.xp;
   avatarUrlInput.value = affectedChar.avatarUrl;
   classesInput.value = "";
-  affectedChar.classes.forEach((element)=>{classesInput.value+=`${element}\n`})
-  submit.classList.add("hidden");
-  modif.classList.remove("hidden");
+  affectedChar.classes.forEach((element) => {
+    classesInput.value += `${element}\n`;
+  });
+  buttonToggle();
+  clearErrors();
   modif.value = id;
 }
-function commitChanges(){
+function cancelChanges() {
+  clear();
+  buttonToggle();
+  modif.value = 0;
+}
+function commitChanges() {
   let id = this.value;
-  if(validateInputs()){
+  if (validateInputs()) {
     modifyCharacter(id);
     writeToLocalStorage();
     modifyRow(id);
-    submit.classList.remove("hidden");
-    modif.classList.add("hidden");
+    buttonToggle();
     clear();
   }
 }
-function modifyCharacter(id){
-  characters[id].name = nameInput.value;
-  characters[id].xp = xpInput.value;
-  characters[id].avatarUrl = avatarUrlInput.value;
+
+function buttonToggle() {
+  submit.classList.toggle("hidden"); //affichage des boutons
+  modif.classList.toggle("hidden");
+  cancel.classList.toggle("hidden");
 }
-function modifyRow(id){
- trToModify = document.getElementById(id);
- let lvlCount = levelCount(characters[id].xp);
- let lvl = lvlCount.lvl;
- let toNextLvl = (characters[id].xp / lvlCount.xpAcc) * 100;
- trToModify.innerHTML = `
-    <td><img src="${characters[id].avatarUrl}" alt=""></td>
-    <td>${characters[id].name}</td>
-    <td>${characters[id].classes}</td>
-    <td>${lvl}</td>
-    <td>${characters[id].xp}</td>
-    <td><div class="progress-bar" ><div class="progress" style="width:${toNextLvl}%"></div></div></td>
-    <td><i class="fas fa-cog" onclick="modify(this)"></i></td>
-    <td><i class="fas fa-times"></i></td>
-    `;
+function modifyCharacter(id) {
+  getById(id).name = nameInput.value;
+  getById(id).xp = xpInput.value;
+  getById(id).avatarUrl = avatarUrlInput.value;
+  getById(id).classes = classesParser();
+  writeToLocalStorage();
+}
+function modifyRow(id) {
+  let charToModify = getById(id);
+  trToModify = document.getElementById(id);
+  trToModify.innerHTML = writeInnerHTML(charToModify);
+}
+/*---- Supprimer une ligne ---*/
+
+function deleteRow(element) {
+  let elementToDelete = element.parentElement.parentElement;
+  if (confirmDelete(element)) {
+    deleteById(elementToDelete.id);
+    elementToDelete.remove();
+    writeToLocalStorage();
+  }
 }
 
+function confirmDelete(row) {
+    let name = getById(row.id).name;
+    window.prompt(`Voulez-vous vraiment supprimer ${name}?`)
+}
+function validTrue(){
+
+}
+/*--- fonctions utiles ---*/
+function getById(id) {
+  return data.characters.find((element) => element.id == id);
+}
+function deleteById(id) {
+  data.characters = data.characters.filter((ele) => ele.id != id);
+}
